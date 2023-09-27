765a6a4432b285d113953473153d9866259a01e5
Buddhika Nipun <bnsbuddhika@gmail.com>
2023-09-27 19:21:00 +0530

    Signup nav bar status dynamic

--- signup/sidebar/script.js
+++ signup/sidebar/script.js
@@ -12,18 +12,18 @@
       ml5 = body.querySelector(".menu-line5"),
       ml6 = body.querySelector(".menu-line6");
 
-      if (!loaded && ml6) {
-        loaded = true;
-        clearInterval(interval);
-      }
-  
+    if (!loaded && ml6) {
+      loaded = true;
+      clearInterval(interval);
+    }
+
     sin.addEventListener("click", () => {
       sin.classList.add("active");
       en.classList.remove("active");
-  
+
       document.documentElement.setAttribute("lang", "sin");
       sessionStorage.setItem("lang", "sin");
-  
+
       ml1.textContent = data["sin"]["ml1"];
       ml2.textContent = data["sin"]["ml2"];
       ml3.textContent = data["sin"]["ml3"];
@@ -31,14 +31,14 @@
       ml5.textContent = data["sin"]["ml5"];
       ml6.innerHTML = data["sin"]["ml6"];
     });
-  
+
     en.addEventListener("click", () => {
       en.classList.add("active");
       sin.classList.remove("active");
-  
+
       document.documentElement.setAttribute("lang", "en");
       sessionStorage.setItem("lang", "en");
-  
+
       ml1.textContent = data["en"]["ml1"];
       ml2.textContent = data["en"]["ml2"];
       ml3.textContent = data["en"]["ml3"];
@@ -46,7 +46,7 @@
       ml5.textContent = data["en"]["ml5"];
       ml6.innerHTML = data["en"]["ml6"];
     });
-  
+
     var data = {
       sin: {
         ml1: "ජයසිංහ ඔයිල් මිල්ස්",
@@ -65,8 +65,42 @@
         ml6: "Already have an account? <a href='../signin.html'>Sign in here.</a>",
       },
     };
-  
+
     checkLng();
   }, 10);
-  })();
-  
+})();
+
+window.addEventListener("load", (e) => {
+  const interval = setInterval(() => {
+    let loaded = false;
+    var pathname = window.location.pathname;
+    pathname = pathname.split("/")[2] || "";
+
+    if (!loaded && pathname) {
+      loaded = true;
+      clearInterval(interval);
+    }
+
+    const vl2 = document.querySelector(".vl2"),
+      circle2 = document.querySelector(".circle2"),
+      ml4 = document.querySelector(".menu-line4"),
+      vl3 = document.querySelector(".vl3"),
+      circle3 = document.querySelector(".circle3"),
+      ml5 = document.querySelector(".menu-line5");
+
+    if (
+      pathname == "signup3.html" ||
+      pathname == "signup4.html" ||
+      pathname == "signup5.html"
+    ) {
+      vl2.classList.remove("disable1");
+      circle2.classList.remove("disable2");
+      ml4.classList.remove("disable3");
+    }
+    if (pathname == "signup4.html" || pathname == "signup5.html") {
+      vl3.classList.remove("disable1");
+      circle3.classList.remove("disable2");
+      ml5.classList.remove("disable3");
+    }
+  }, 10);
+});
