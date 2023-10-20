a03a855e475f59701a197a595d60c82c4ba60daa
Buddhika Nipun <bnsbuddhika@gmail.com>
2023-10-21 00:11:21 +0530

    yard time and date added

--- supplier/supply/yard-cash.js
+++ supplier/supply/yard-cash.js
@@ -4,6 +4,8 @@
     en = body.querySelector(".en"),
     sTitle = body.querySelector(".supply-title"),
     tText = body.querySelector(".top-text"),
+    date = body.querySelector(".date"),
+    time = body.querySelector(".time"),
     btn = body.querySelector(".form-button");
 
   sin.addEventListener("click", () => {
@@ -16,6 +18,8 @@
 
     sTitle.textContent = data["sin"]["sTitle"];
     tText.innerHTML = data["sin"]["tText"];
+    date.placeholder = data["sin"]["date"];
+    time.placeholder = data["sin"]["time"];
     btn.textContent = data["sin"]["btn"];
   });
 
@@ -29,6 +33,8 @@
 
     sTitle.textContent = data["en"]["sTitle"];
     tText.innerHTML = data["en"]["tText"];
+    date.placeholder = data["en"]["date"];
+    time.placeholder = data["en"]["time"];
     btn.textContent = data["en"]["btn"];
   });
 
@@ -37,46 +43,71 @@
       sTitle: "නව සැපයුම",
       tText:
         "ඔබගේ නව සැපයුම් ඉල්ලීම සඳහා විස්තර නිවැරදිව පුරවන්න. <br />ඉල්ලීම් පැය 24ක් ඇතුළත සමාලෝචනය කෙරේ. ඔබට ඒවා ඔබේ උපකරණ පුවරුව තුළ පරීක්ෂා කළ හැකිය.",
+      date: "හැකි දිනය",
+      time: "හැකි කාලය",
       btn: "ඉල්ලීම ඉදිරිපත් කරන්න",
     },
     en: {
       sTitle: "New Supply",
       tText:
         "Fill up the details correctly for your new supply request. <br />Requests will be reviewed within 24 hours. You can check them inside  your dashboard.",
+      date: "feasible date",
+      time: "feasible time",
       btn: "Submit Request",
     },
   };
 
   btn.addEventListener("click", () => {
-    var formData = {
-      // collection_id: sessionStorage.getItem("id"),
-      // supplier_id: sessionStorage.getItem("sId"),
-      collection_id: getCookie("id"),
-      supplier_id: getCookie("sId"),
-    };
-    fetch(backProxy + "/yard", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(formData),
-      credentials: "include",
-    })
-      .then((response) => {
-        if (response.status == 200) {
-          response.json().then((data) => {
-            console.log(data.message);
-          });
-        } else if (response.status === 400) {
-          response.json().then((data) => {
-            console.log(data.message);
-          });
-        } else {
-          console.error("Error:", response.status);
-        }
+    var dateStatus = false,
+      timeStatus = false;
+
+    if (typeof date.value === "string" && date.value.trim().length === 0) {
+      console.log("Date cannot be empty");
+      date.focus();
+    } else {
+      dateStatus = true;
+    }
+
+    if (typeof time.value === "string" && time.value.trim().length === 0) {
+      console.log("Time cannot be empty");
+      time.focus();
+    } else {
+      timeStatus = true;
+    }
+
+    if (dateStatus && timeStatus) {
+      var formData = {
+        // collection_id: sessionStorage.getItem("id"),
+        // supplier_id: sessionStorage.getItem("sId"),
+        collection_id: getCookie("id"),
+        supplier_id: getCookie("sId"),
+        date: date.value,
+        time: time.value,
+      };
+      fetch(backProxy + "/yard", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+        credentials: "include",
       })
-      .catch((error) => {
-        console.error("An error occurred:", error);
-      });
+        .then((response) => {
+          if (response.status == 200) {
+            response.json().then((data) => {
+              console.log(data.message);
+            });
+          } else if (response.status === 400) {
+            response.json().then((data) => {
+              console.log(data.message);
+            });
+          } else {
+            console.error("Error:", response.status);
+          }
+        })
+        .catch((error) => {
+          console.error("An error occurred:", error);
+        });
+    }
   });
 })();
