9edd808457f8aae284f5cc9c10eb12bec2f943ed
Buddhika Nipun <bnsbuddhika@gmail.com>
2023-12-10 22:31:10 +0530

    Invoice for supplier

--- supplier/reports/report.js
+++ supplier/reports/report.js
@@ -114,6 +114,153 @@
       console.error("An error occurred:", error);
       Command: toastr["error"](error);
     });
+
+    //pdf generate code
+  //Generate pdf
+  var pdfObject; //outputType: jsPDFInvoiceTemplate.OutputType.Blob,
+
+  var props = {
+    outputType: jsPDFInvoiceTemplate.OutputType.Blob,
+    returnJsPDFDocObject: true,
+    fileName: "Invoice 2023",
+    orientationLandscape: false,
+    compress: true,
+    logo: {
+      src: "../../common/JOM logo 1.png",
+      type: "PNG", //optional, when src= data:uri (nodejs case)
+      width: 53.33, //aspect ratio = width/height
+      height: 26.66,
+      margin: {
+        top: 0, //negative or positive num, from the current position
+        left: 0, //negative or positive num, from the current position
+      },
+    },
+    stamp: {
+      inAllPages: true, //by default = false, just in the last page
+      src: "https://raw.githubusercontent.com/edisonneza/jspdf-invoice-template/demo/images/qr_code.jpg",
+      type: "JPG", //optional, when src= data:uri (nodejs case)
+      width: 20, //aspect ratio = width/height
+      height: 20,
+      margin: {
+        top: 0, //negative or positive num, from the current position
+        left: 0, //negative or positive num, from the current position
+      },
+    },
+    business: {
+      name: "Jayasinghe Oil Mills",
+      address: "No 105, Ravita Road, Welpalla, Sri Lanka.",
+      phone: "(+94) 76 924 0963",
+      email: "jom@jom.com",
+      email_1: "jmyasiru@gmail.com",
+      website: "www.jom.com",
+    },
+    contact: {
+      label: "Invoice issued for:",
+      name: "Stock Manager",
+      address: "NO 858, Pannala, Kuliyapitiya",
+      phone: "(+94) 71 22 22 222",
+      email: "kamal@gmail.com",
+      otherInfo: "www.jom.com",
+    },
+    invoice: {
+      label: "Invoice #: ",
+      num: 19,
+      invDate: "Payment Date: 25/10/2023 18:12",
+      invGenDate: "Invoice Date: 26/10/2023 10:17",
+      headerBorder: false,
+      tableBodyBorder: false,
+      header: [
+        {
+          title: "#",
+          style: {
+            width: 10,
+          },
+        },
+        {
+          title: "Title",
+          style: {
+            width: 30,
+          },
+        },
+        {
+          title: "Description",
+          style: {
+            width: 80,
+          },
+        },
+        { title: "Price" },
+        { title: "Quantity" },
+        { title: "Unit" },
+        { title: "Total" },
+      ],
+      table: Array.from(Array(10), (item, index) => [
+        index + 1,
+        "There are many variations ",
+        "Lorem Ipsum is simply dummy text dummy text ",
+        200.5,
+        4.5,
+        "m2",
+        400.5,
+      ]),
+      additionalRows: [
+        {
+          col1: "Total:",
+          col2: "145,250.50",
+          col3: "ALL",
+          style: {
+            fontSize: 14, //optional, default 12
+          },
+        },
+        {
+          col1: "VAT:",
+          col2: "20",
+          col3: "%",
+          style: {
+            fontSize: 10, //optional, default 12
+          },
+        },
+        {
+          col1: "SubTotal:",
+          col2: "116,199.90",
+          col3: "ALL",
+          style: {
+            fontSize: 10, //optional, default 12
+          },
+        },
+      ],
+      invDescLabel: "Invoice Note",
+      invDesc:
+        "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary.",
+    },
+    footer: {
+      text: "The invoice is created on a computer and is valid without the signature and stamp.",
+    },
+    pageEnable: true,
+    pageLabel: "Page ",
+  };
+
+  /* generate pdf */
+  function generatePDF() {
+    pdfObject = jsPDFInvoiceTemplate.default(props);
+    console.log("Object generated: ", pdfObject);
+    viewPDF();
+  }
+
+  /* view pdf */
+  function viewPDF() {
+    if (!pdfObject) {
+      Command: toastr["error"]("No PDF Object");
+      return console.log("No PDF Object");
+    }
+
+    var fileURL = URL.createObjectURL(pdfObject.blob);
+    window.open(fileURL, "_blank");
+  }
+
+  const genBtn = document.querySelector(".form-button");
+  genBtn.addEventListener("click", () => {
+    generatePDF();
+  });
 })();
 
 function rateChart(coco) {
