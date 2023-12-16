c1b0ae52adc10c34452661aea9f83648a8a16226
Saumya Sewwandi <122492063+BMSaumya@users.noreply.github.com>
2023-12-16 13:54:50 +0530

    added forgot password page

---
+++ forgot-password/load-sidebar.js
@@ -0,0 +1,15 @@
+if (getCookie("jwt") != null && getCookie("jwt").length != 0)
+  window.location.href = frontProxy + "/" + getPayload(getCookie("jwt")).page;
+
+(() => {
+  const sidebar = document.querySelector(".side");
+  fetch("./sidebar.html")
+    .then((res) => res.text())
+    .then((data) => {
+      sidebar.innerHTML = data;
+    });
+
+  const sideScript = document.createElement("script");
+  sideScript.setAttribute("src", "./sidebar.js");
+  document.body.appendChild(sideScript);
+})();
