4fbc92b1eebaa426a34ed960286a18286ee04a10
Saumya Sewwandi <122492063+BMSaumya@users.noreply.github.com>
2023-12-25 01:04:48 +0530

    added products window

---
+++ sales-manager/products/view-all.js
@@ -0,0 +1,67 @@
+(() => {
+  const body = document.querySelector("body"),
+    sin = body.querySelector(".sin"),
+    en = body.querySelector(".en"),
+    pTitle = body.querySelector(".productionmg-title"),
+    pText = body.querySelector(".productionmg-text"),
+    closeBtn = body.querySelector(".close-btn-bank"),
+    overlay = body.querySelector(".overlay"),
+    add = body.querySelector(".add");
+
+  var lang = getCookie("lang"); // current language
+
+  overlay.addEventListener("click", (e) => {
+    if (e.target.id === "overlay") {
+      overlay.style.display = "none";
+      document.querySelector(".view-product-container").style.display = "none";
+    }
+  });
+
+  closeBtn.addEventListener("click", () => {
+    overlay.style.display = "none";
+    document.querySelector(".view-product-container").style.display = "none";
+  });
+
+  sin.addEventListener("click", () => {
+    sin.classList.add("active");
+    en.classList.remove("active");
+
+    document.documentElement.setAttribute("lang", "sin");
+    document.cookie = "lang=sin; path=/";
+    lang = "sin";
+
+    pTitle.textContent = data["sin"]["pTitle"];
+    pText.innerHTML = data["sin"]["pText"];
+
+    add.textContent = data["sin"]["add"];
+
+    setGreeting();
+  });
+
+  en.addEventListener("click", () => {
+    en.classList.add("active");
+    sin.classList.remove("active");
+
+    document.documentElement.setAttribute("lang", "en");
+    document.cookie = "lang=en; path=/";
+    lang = "en";
+
+    pTitle.textContent = data["en"]["pTitle"];
+    pText.innerHTML = data["en"]["pText"];
+    add.textContent = data["en"]["add"];
+    setGreeting();
+  });
+
+  var data = {
+    sin: {
+      pTitle: "නිෂ්පාදන",
+      pText: "සමාගමේ නිෂ්පාදන විස්තර බලන්න සහ සංස්කරණය කරන්න",
+      add: "අලුතින් එකතු කරන්න",
+    },
+    en: {
+      pTitle: "Products",
+      pText: "View and edit company product details",
+      add: "Add New",
+    },
+  };
+})();
