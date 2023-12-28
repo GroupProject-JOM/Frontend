65943c84e1bb0113d25f39ab4fa36a112c1f07a4
Saumya Sewwandi <122492063+BMSaumya@users.noreply.github.com>
2023-12-28 10:13:42 +0530

    production view page

---
+++ sales-manager/productions/view.js
@@ -0,0 +1,18 @@
+(() => {
+    const body = document.querySelector("body"),
+    sin = body.querySelector(".sin"),
+    en = body.querySelector(".en"),
+    // pTitle = body.querySelector(".productionmg-title"),
+    // pText1 = body.querySelector(".productionmg-text1"),
+    overlay = body.querySelector(".overlay"),
+    product = body.querySelector(".product"),
+    closeBtn1 = body.querySelector(".close-btn1"),
+    closeBtn2 = body.querySelector(".close-btn2");
+    // save = body.querySelector(".save"),
+
+    
+
+
+  
+    
+})();
