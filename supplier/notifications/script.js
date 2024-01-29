df744ae2e10b504dc308498c7a76346e19d6a96d
Buddhika Nipun <bnsbuddhika@gmail.com>
2024-01-29 21:58:53 +0530

    Supplier notification done

--- supplier/notifications/script.js
+++ supplier/notifications/script.js
@@ -9,6 +9,7 @@
       bell = body.querySelector(".bell"),
       fabell = body.querySelector(".fa-bell"),
       notify = body.querySelector("#notify"),
+      count = document.querySelector(".count"),
       l10 = body.querySelector(".l10");
 
     if (!loaded && l10) {
@@ -26,12 +27,52 @@
 
     bell.addEventListener("click", () => {
       notify.style.display = "grid";
+
+      var formData = {
+        notifications: unSeenArray,
+      };
+
+      count.textContent = +count.textContent - unSeenArray.length;
+      if (+count.textContent <= 0) count.textContent = null;
+
+      if (unseenCount != 0) {
+        fetch(backProxy + "/notification", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(formData),
+          credentials: "include",
+        })
+          .then((response) => {
+            if (response.status == 200) {
+              response.json().then((data) => {});
+            } else if (response.status === 202) {
+              response.json().then((data) => {
+                console.log(data.notifications);
+              });
+              if (lang == "sin") Command: toastr["info"]("දැනුම්දීම් නැත");
+              else Command: toastr["info"]("No notifications");
+            } else if (response.status === 401) {
+              response.json().then((data) => {
+                console.log(data.message);
+              });
+              if (lang == "sin") Command: toastr["error"]("වලංගු නොවන පරිශීලක");
+              else Command: toastr["error"]("Invalid User");
+            } else {
+              console.error("Error:", response.status);
+              Command: toastr["error"](response.status, "Error");
+            }
+          })
+          .catch((error) => {
+            console.error("An error occurred:", error);
+            Command: toastr["error"](error);
+          });
+      }
     });
 
     body.addEventListener("click", (e) => {
-      if (
-        !e.target.classList.contains("do-not")
-      ) {
+      if (!e.target.classList.contains("do-not")) {
         notify.style.display = "none";
         // bell.classList.remove("active");
         fabell.classList.add("far");
@@ -54,10 +95,8 @@
     closeBtn.addEventListener("click", () => {
       notify.style.display = "none";
       fabell.classList.add("far");
-        fabell.classList.remove("fas");
+      fabell.classList.remove("fas");
     });
-
-
 
     var data = {
       sin: {
@@ -69,4 +108,63 @@
     };
     checkLng();
   }, 10);
+
+  var row = "",
+    unseenCount = 0,
+    unSeenArray = [];
+
+  const notifications = document.querySelector("#notifications"),
+    count = document.querySelector(".count");
+
+  const interval1 = setInterval(() => {
+    fetch(backProxy + "/notification", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      credentials: "include",
+    })
+      .then((response) => {
+        if (response.status == 200) {
+          response.json().then((data) => {
+            let arr = data.notifications;
+            arr.forEach(data_to_table);
+
+            function data_to_table(item) {
+              if (item.status == 0) {
+                unseenCount++;
+                unSeenArray.push(item.id);
+              }
+              row += ` <div id=${item.id} class="notify-msg do-not">${
+                item.message
+              }<span>${formatTimeDifference(item.time)}</span></div>`;
+            }
+
+            notifications.innerHTML = row;
+            count.textContent = unseenCount;
+
+            if (unseenCount == 0) count.textContent = null;
+          });
+        } else if (response.status === 202) {
+          response.json().then((data) => {
+            console.log(data.notifications);
+          });
+          if (lang == "sin") Command: toastr["info"]("දැනුම්දීම් නැත");
+          else Command: toastr["info"]("No notifications");
+        } else if (response.status === 401) {
+          response.json().then((data) => {
+            console.log(data.message);
+          });
+          if (lang == "sin") Command: toastr["error"]("වලංගු නොවන පරිශීලක");
+          else Command: toastr["error"]("Invalid User");
+        } else {
+          console.error("Error:", response.status);
+          Command: toastr["error"](response.status, "Error");
+        }
+      })
+      .catch((error) => {
+        console.error("An error occurred:", error);
+        Command: toastr["error"](error);
+      });
+  }, 300000);
 })();
